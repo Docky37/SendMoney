14a84ce4d0d3f809fbb410dd64c2831757bc80df
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-06-23 00:09:08 +0200

    fix: component annotation forgotten in RetrieveMail class

--- src/main/java/com/paymybuddy/sendmoney/security/util/EmailRetrieve.java
+++ src/main/java/com/paymybuddy/sendmoney/security/util/EmailRetrieve.java
@@ -2,10 +2,12 @@ package com.paymybuddy.sendmoney.security.util;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Thierry SCHREINER
  */
+@Component
 public class EmailRetrieve {
 
     public  String getEmail() {
