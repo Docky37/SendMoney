f026dc98dde8c4260625d05786d99b44cc6f0ba3
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-06-23 01:15:56 +0200

    feat: add AddConnectionServiceTest

---
+++ src/main/java/com/paymybuddy/sendmoney/moneyaccounts/service/AddConnectionService.java
@@ -0,0 +1,10 @@
+package com.paymybuddy.sendmoney.moneyaccounts.service;
+
+/**
+ * @author Thierry SCHREINER
+ */
+public interface AddConnectionService {
+
+    public void addConnection(String eMail); 
+   
+}
---
+++ src/main/java/com/paymybuddy/sendmoney/moneyaccounts/service/AddConnectionServiceImpl.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.sendmoney.moneyaccounts.service;
+
+/**
+ * @author Thierry SCHREINER
+ */
+public class AddConnectionServiceImpl implements AddConnectionService{
+
+    @Override
+    public void addConnection(final String eMail) {
+        
+    }
+
+}
