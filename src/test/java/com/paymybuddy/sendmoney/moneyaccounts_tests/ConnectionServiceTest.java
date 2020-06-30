a5828343d7142649e7b5df04d5a9da50849a6e63
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-06-30 11:15:43 +0200

    feat: modify connection functionnality
    modify ConnectionController to use EmailDTO & UserWithoutPmbAccountException
    modify ConnectionService to use UserWithoutPmbAccountException
    adapt  Unit tests

--- src/test/java/com/paymybuddy/sendmoney/moneyaccounts_tests/ConnectionServiceTest.java
+++ src/test/java/com/paymybuddy/sendmoney/moneyaccounts_tests/ConnectionServiceTest.java
@@ -8,7 +8,8 @@ import static org.mockito.Mockito.verify;
 import java.util.TreeSet;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.never; 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -22,6 +23,7 @@ import com.paymybuddy.sendmoney.moneyaccounts.service.ConnectionService;
 import com.paymybuddy.sendmoney.moneyaccounts.util.PmbAccountMapping;
 import com.paymybuddy.sendmoney.security.model.Buddy;
 import com.paymybuddy.sendmoney.security.util.EmailRetrieve;
+import com.paymybuddy.sendmoney.moneyaccounts.exception.UserWithoutPmbAccountException;
 import com.paymybuddy.sendmoney.moneyaccounts.model.PmbAccount;
 import com.paymybuddy.sendmoney.moneyaccounts.model.PmbAccountDTO;
 
@@ -53,7 +55,7 @@ public class ConnectionServiceTest {
 
     @Test // AddConnection method
     public void givenAnEmail_whenAddConnection_thenConnectionIsAdded()
-            throws Exception {
+            throws Exception, UserWithoutPmbAccountException {
         // GIVEN
         String eMail = "Yo.Yo@yoyo.fr";
         Buddy owner = new Buddy();
@@ -68,14 +70,15 @@ public class ConnectionServiceTest {
         connectionService.addConnection(eMail);
         // THEN
         verify(pmbAccountRepository, times(2)).findByOwnerEmail(anyString());
+        verify(emailRetrieve).getEmail();
         verify(pmbAccountRepository).save(any(PmbAccount.class));
         verify(pmbAccountMapping).mapPmbAccountToDTO(any(PmbAccount.class));
         verify(pmbAccount).addConnection(any(PmbAccount.class));
-   }
+    }
 
     @Test // AddConnection method
     public void givenNonRegistredEmail_whenAddConnection_thenConnectionIsAdded()
-            throws Exception {
+            throws Exception, UserWithoutPmbAccountException {
         // GIVEN
         String eMail = "Yo.Yo@yoyo.fr";
         given(pmbAccountRepository.findByOwnerEmail(anyString()))
@@ -92,9 +95,24 @@ public class ConnectionServiceTest {
         assertThat(pmbAccountDTO).isEqualTo(null);
     }
 
+    @Test // AddConnection method
+    public void givenNoPmbAccount_whenAddConnection_thenThrowsException()
+            throws Exception, UserWithoutPmbAccountException {
+        // GIVEN
+        String eMail = "Yo.Yo@yoyo.fr";
+        given(pmbAccountRepository.findByOwnerEmail(anyString()))
+                .willReturn(pmbAccount, (PmbAccount) null);
+        given(emailRetrieve.getEmail()).willReturn("My.Mail@pmb.fr");
+        // WHEN
+        // THEN
+        assertThrows(UserWithoutPmbAccountException.class,
+                () -> connectionService.addConnection(eMail));
+        verify(pmbAccountRepository, times(2)).findByOwnerEmail(anyString());
+    }
+
     @Test // DelConnection method
     public void givenAnEmail_whenDelConnection_thenConnectionIsDeleted()
-            throws Exception {
+            throws Throwable {
         // GIVEN
         String eMail = "Yo.Yo@yoyo.fr";
         pmbAccount.setId(1L);
@@ -109,6 +127,7 @@ public class ConnectionServiceTest {
         connectionService.delConnection(eMail);
         // THEN
         verify(pmbAccountRepository, times(2)).findByOwnerEmail(anyString());
+        verify(emailRetrieve).getEmail();
         verify(pmbAccountRepository).save(any(PmbAccount.class));
         verify(pmbAccountMapping).mapPmbAccountToDTO(any(PmbAccount.class));
         verify(pmbAccount).getConnections();
@@ -116,22 +135,37 @@ public class ConnectionServiceTest {
 
     @Test // DelConnection method
     public void givenNonRegistredEmail_whenDelConnection_thenConnectionIsDeleted()
-            throws Exception {
+            throws Exception, UserWithoutPmbAccountException {
         // GIVEN
         String eMail = "Yo.Yo@yoyo.fr";
         given(pmbAccountRepository.findByOwnerEmail(anyString()))
                 .willReturn(null);
         // WHEN
         PmbAccountDTO pmbAccountDTO = connectionService.delConnection(eMail);
-       // THEN
+        // THEN
         verify(pmbAccountRepository).findByOwnerEmail(anyString());
         verify(pmbAccountRepository, never()).save(any(PmbAccount.class));
         assertThat(pmbAccountDTO).isEqualTo(null);
     }
 
+    @Test // DelConnection method
+    public void givenNoPmbAccount_whenDelConnection_thenThrowsException()
+            throws Exception, UserWithoutPmbAccountException {
+        // GIVEN
+        String eMail = "Yo.Yo@yoyo.fr";
+        given(pmbAccountRepository.findByOwnerEmail(anyString()))
+                .willReturn(pmbAccount, (PmbAccount) null);
+        given(emailRetrieve.getEmail()).willReturn("My.Mail@pmb.fr");
+        // WHEN
+        // THEN
+        assertThrows(UserWithoutPmbAccountException.class,
+                () -> connectionService.delConnection(eMail));
+        verify(pmbAccountRepository, times(2)).findByOwnerEmail(anyString());
+    }
+
     @Test // GetConnections method
-    public void givenAUser_whenGetConnections_thenReturnsTheListOfConnections()
-            throws Exception {
+    public void givenAUser_whenGetConnections_thenReturnsHisPmbAccountDTO()
+            throws Exception, UserWithoutPmbAccountException {
         // GIVEN
         given(pmbAccountRepository.findByOwnerEmail(anyString()))
                 .willReturn(new PmbAccount());
@@ -141,8 +175,27 @@ public class ConnectionServiceTest {
         // WHEN
         connectionService.getConnections();
         // THEN
+        verify(emailRetrieve).getEmail();
         verify(pmbAccountRepository).findByOwnerEmail(anyString());
         verify(pmbAccountMapping).mapPmbAccountToDTO(any(PmbAccount.class));
+    }
+
+    @Test // GetConnections method
+    public void givenAUserWithoutPmbAccount_whenGetConnections_thenReturnsNull()
+            throws Exception, UserWithoutPmbAccountException {
+        // GIVEN
+        given(pmbAccountRepository.findByOwnerEmail(anyString()))
+                .willReturn(null);
+        given(emailRetrieve.getEmail()).willReturn("My.Mail@pmb.fr");
+        given(pmbAccountMapping.mapPmbAccountToDTO(any(PmbAccount.class)))
+                .willReturn(new PmbAccountDTO());
+        // WHEN
+        connectionService.getConnections();
+        // THEN
+        verify(pmbAccountRepository).findByOwnerEmail(anyString());
+        verify(emailRetrieve).getEmail();
+        verify(pmbAccountMapping, never())
+                .mapPmbAccountToDTO(any(PmbAccount.class));
     }
 
 }
