248d6c0868e5e3051d4d1d955f02cba8b86a1521
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-07-15 22:57:06 +0200

    feat: create WithdrawalControllerTest

---
+++ src/main/java/com/paymybuddy/sendmoney/money_transfer/service/WithdrawalService.java
@@ -0,0 +1,30 @@
+package com.paymybuddy.sendmoney.money_transfer.service;
+
+import com.paymybuddy.sendmoney.money_transfer.model.OrderDTO;
+import com.paymybuddy.sendmoney.money_transfer.model.Transfer;
+
+/**
+ * @author Thierry SCHREINER
+ *
+ */
+public class WithdrawalService {
+
+    /**
+     * @param any
+     * @return
+     */
+    public Transfer send(OrderDTO order) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * @param any
+     * @return
+     */
+    public String saveTransaction(Transfer withdrawal) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/test/java/com/paymybuddy/sendmoney/moneytransfer_tests/WithdrawalControllerTest.java
@@ -0,0 +1,70 @@
+package com.paymybuddy.sendmoney.moneytransfer_tests;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.paymybuddy.sendmoney.money_transfer.model.OrderDTO;
+import com.paymybuddy.sendmoney.money_transfer.model.Transfer;
+import com.paymybuddy.sendmoney.money_transfer.service.WithdrawalService;
+
+/**
+ * @author Thierry SCHREINER
+ */
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class WithdrawalControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private WithdrawalService withdrawalService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test // PostMapping("/withdrawal")
+    public void givenAValidOrderDTO_whenSend_thenWithdrawalIsCreated()
+            throws Exception {
+        given(withdrawalService.send(any(OrderDTO.class)))
+                .willReturn(new Transfer());
+        given(withdrawalService.saveTransaction(any(Transfer.class)))
+                .willReturn("201 Created");
+        mvc.perform(MockMvcRequestBuilders.post("/pmb-adm/deposit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"email\":\"my.beneficiary@pmb.fr\","
+                        + "\"amount\":100.00}"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn();
+
+         verify(withdrawalService).send(any(OrderDTO.class));
+
+    }
+
+}
